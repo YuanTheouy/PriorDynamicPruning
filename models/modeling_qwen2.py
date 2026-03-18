c1ad350b901cf720acc820adc129ad77916debfa
yuanxin.theo <yuanxin.theo@bytedance.com>
2026-03-18 15:53:41 +0800

    fix

--- models/modeling_qwen2.py
+++ models/modeling_qwen2.py
@@ -489,7 +489,7 @@ class Qwen2ForCausalLM(Qwen2PreTrainedModel, GenerationMixin):
         if past_key_values is not None:
             if isinstance(past_key_values, Cache):
                 cache_length = past_key_values.get_seq_length()
-                past_length = past_key_values.seen_tokens
+                past_length = past_key_values.get_seq_length()
                 max_cache_length = past_key_values.get_max_length()
             else:
                 cache_length = past_length = past_key_values[0][0].shape[2]

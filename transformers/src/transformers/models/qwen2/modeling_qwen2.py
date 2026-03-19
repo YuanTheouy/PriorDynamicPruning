dceafc1e17c2718f48420ec44380ab673c221bf7
yuanxin.theo <yuanxin.theo@bytedance.com>
2026-03-19 11:54:59 +0800

    fix

--- transformers/src/transformers/models/qwen2/modeling_qwen2.py
+++ transformers/src/transformers/models/qwen2/modeling_qwen2.py
@@ -4,7 +4,7 @@
 #             the file from the modular. If any change should be done, please apply the change to the
 #                          modular_qwen2.py file directly. One of our CI enforces this.
 #                🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
-from typing import Callable, Optional, Union
+from typing import Callable, Optional, Union, Tuple
 
 import torch
 from torch import nn
